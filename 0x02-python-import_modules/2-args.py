a9c4880ea2c3ddacba0aff737bc958feab514988
Muhadev <muhammedfayemi@gmail.com>
2023-11-06 03:08:08 -0800

    2-args.py is added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+def print_str(str):
+    print("{}".format(str))
 
 if __name__ == "__main":
     import sys
 
-    num_args = len(sys.argv) - 1
-    if num_args == 0:
+    num_args = len(sys.argv)
+    if num_args == 1:
         print("0 arguments.")
-    elif num_args == 1:
+    elif num_args == 2:
         print("1 arguments:")
-    else:
-        print("{} arguments:".format(count))
+        print("{:d}: ".format(1), end="")
+        print_str(str(sys.argv[1]))
+    elif num_args > 2:
+        print("{} arguments:".format(num_args - 1))
 
-    for i in range(num_args):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(1, num_args):
+        print("{:d}: ".format(i), end="")
+        print_str(str(sys.argv[i]))
