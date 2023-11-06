59d1a3d5591349dcd797be617672f64693c76af0
Muhadev <muhammedfayemi@gmail.com>
2023-11-06 14:39:40 -0800

    10-main.py is added

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []  # Initialize an empty list to store the boolean values
-
-    for num in my_list:
-        is_divisible = num % 2 == 0  # Check if the number is divisible by 2
-        result.append(is_divisible)  # Append the boolean value to the result list
-
-    return result
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
