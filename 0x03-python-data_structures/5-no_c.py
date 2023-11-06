4265811c3789ed374e79f5b420d6a6d4fec88792
Muhadev <muhammedfayemi@gmail.com>
2023-11-06 14:03:43 -0800

    5-main.py is added

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     result = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            result = result + char
+    for char in range(len(my_string)):
+        if my_string[char] != 'c' and my_string[char] != 'C':
+            result += my_string[char]
     return result
