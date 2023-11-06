6a76f74fbb11b30e2eb81cea1538a3c543fc54e4
Muhadev <muhammedfayemi@gmail.com>
2023-11-06 08:21:03 -0800

    5-variable_load.py is added

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,4 +2,10 @@
 
 if __name__ == "__main__":
     from variable_load_5 import a  # Import the variable 'a' from variable_load_5.py
-    print(a)  # Print the value of the variable 'a'
+    if a >= 0:
+        sign = ""
+    else:
+        sign = "-"
+        a = abs(a)
+
+    print(a)
