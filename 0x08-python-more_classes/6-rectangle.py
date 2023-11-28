60c6f8f3db0c952f9f30dffc4c56979bf20fe0b1
Muhadev <muhammedfayemi@gmail.com>
2023-11-28 08:40:31 -0800

    6-rectangle.py is added

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,7 +6,9 @@
 
 class Rectangle:
     """Representation of a rectangle"""
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
