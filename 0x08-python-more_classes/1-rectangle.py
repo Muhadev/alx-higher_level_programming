48483bddc44a869a992f7446ac286bd55f6897a7
Muhadev <muhammedfayemi@gmail.com>
2023-11-27 22:32:18 -0800

    1-rectangle.py is added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,9 +2,9 @@
 
 """class Rectangle that defines a rectangle"""
 
+
 class Rectangle:
     """class Rectangle that defines a rectangle"""
-
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -30,6 +30,6 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif  value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
