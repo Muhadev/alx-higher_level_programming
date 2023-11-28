807d5ced359eea03c0bcb46ffd5cf9e4db0aa687
Muhadev <muhammedfayemi@gmail.com>
2023-11-28 08:34:34 -0800

    6-rectangle.py is added

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+    number_of_instances = 0
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for _ in range(self.__height))
+        return string
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.width}, {self.height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
