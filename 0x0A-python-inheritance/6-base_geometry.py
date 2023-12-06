286cea8c9e2c6b8df1da5640e251539d6b6988cd
Muhadev <muhammedfayemi@gmail.com>
2023-12-06 03:53:43 -0800

    6-base_geometry.py is added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+ a function that returns True if the object
+ is exactly an instance of the specified class
+  otherwise False.
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
