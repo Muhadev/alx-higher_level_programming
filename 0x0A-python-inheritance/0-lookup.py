7159e687e16747dbe15e5b629082dff091d308f0
Muhadev <muhammedfayemi@gmail.com>
2023-12-06 02:30:35 -0800

    0-lookup.py is added

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""returns the list of available attributes and method"""
+
+
+def lookup(obj):
+
+    """returns the list of available attributes and method"""
+    return dir(obj)
