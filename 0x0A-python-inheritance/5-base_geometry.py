1a61aa297d07ad6ba1613306ef8f0a971d469195
Muhadev <muhammedfayemi@gmail.com>
2023-12-06 03:50:15 -0800

    5-base_geometry.py is added

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+ a function that returns True if the object
+ is exactly an instance of the specified class
+  otherwise False.
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
