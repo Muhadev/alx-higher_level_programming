47279ee8c00cb9425d7ca0a32ce615e599eb95e3
Muhadev <muhammedfayemi@gmail.com>
2023-12-06 19:21:37 -0800

    2-append_write.py is added

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@ function that reads a text file
 """
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.append(), end="")
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
