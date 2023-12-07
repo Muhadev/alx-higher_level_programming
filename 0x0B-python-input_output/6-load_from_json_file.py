8015901e32c43933fb935a37391f8ed9eddf0fc9
Muhadev <muhammedfayemi@gmail.com>
2023-12-07 00:12:52 -0800

    6-load_from_json_file.py is added

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that reads a text file
+(UTF8) and prints it to stdout
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        json.load(f)
