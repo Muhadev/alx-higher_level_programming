b83c075b6a01eacc5037564a0aa18c3a5577a71b
Muhadev <muhammedfayemi@gmail.com>
2023-12-08 08:06:15 -0800

    12-pascal_triangle.py is added

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+function that reads a text file
+(UTF8) and prints it to stdout
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([last_row[j] + last_row[j + 1]
+                        for j in range(len(last_row) - 1)])
+            row.append(1)
+        triangle.append(row)
+    return triangle
