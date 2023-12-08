d48bc1bc618a956859e7ac56609ebdb83d13a3a1
Muhadev <muhammedfayemi@gmail.com>
2023-12-08 12:12:48 -0800

    100-append_after.py is added

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that reads a text file
+(UTF8) and prints it to stdout
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that inserts a line of text to a file"""
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
