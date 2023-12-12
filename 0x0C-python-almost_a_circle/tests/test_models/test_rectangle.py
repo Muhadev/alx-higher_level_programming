26d03b9d105203bc27e808212c882b11b10dcc82
Muhadev <muhammedfayemi@gmail.com>
2023-12-11 17:03:25 -0800

    ractangle.py is addded

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+classes and methods must be unit tested
+and be PEP 8 validated.
+"""
+import unittest
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+    """Test object creation and attribute assignment"""
+    def test_initialization(self):
+        """Test if the ID is assigned when an ID is provided"""
+        rect = Rectangle(10, 20, 3, 5, 1)
+        self.assertEqual(rect.width, 10)
+        self.assertEqual(rect.height, 20)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 5)
+        self.assertEqual(rect.id, 1)
+
+    def test_attribute_setters(self):
+        """Test if the ID increments when no ID is provided"""
+        rect = Rectangle(10, 20)
+        rect.width = 15
+        rect.height = 25
+        rect.x = 5
+        rect.y = 10
+        self.assertEqual(rect.width, 15)
+        self.assertEqual(rect.height, 25)
+        self.assertEqual(rect.x, 5)
+        self.assertEqual(rect.y, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
