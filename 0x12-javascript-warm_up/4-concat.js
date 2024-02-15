477bbf1e12ee2000446b1578328708ad89a75482
Muhadev <muhammedfayemi@gmail.com>
2024-02-15 01:49:18 -0800

    4-concat.js added

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argument1 = process.argv[2];
+const argument2 = process.argv[3];
+
+if (argument1 !== undefined && argument2 !== undefined) {
+  console.log(`${argument1} is ${argument2}`);
+} else {
+  console.log('Two arguments are required.');
+}
