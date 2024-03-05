8b8f4fdb92d4a8798085214cc9752c8578aae139
Muhadev <muhammedfayemi@gmail.com>
2024-03-04 16:26:47 -0800

    5-post_params.sh added

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Check if the URL argument is provided
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1")
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
