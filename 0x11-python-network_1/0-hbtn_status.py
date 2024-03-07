f712c406f50998d5d415251665ac444dbae18ffc
Muhadev <muhammedfayemi@gmail.com>
2024-03-07 13:50:06 -0800

    task is added

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    utf8_content = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", utf8_content)
