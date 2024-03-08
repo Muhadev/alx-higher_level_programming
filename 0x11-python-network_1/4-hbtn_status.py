83d24e11321a17f08070c25ce4a1351e56faf79a
Muhadev <muhammedfayemi@gmail.com>
2024-03-07 16:22:40 -0800

    task is added

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status.
+Uses the package requests.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
