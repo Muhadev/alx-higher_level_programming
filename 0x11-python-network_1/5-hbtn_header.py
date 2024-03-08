bef45e7b14c88c16a6eaf80e0afa30c05fb9b202
Muhadev <muhammedfayemi@gmail.com>
2024-03-07 16:34:42 -0800

    task is added

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,8 +12,5 @@ if __name__ == "__main__":
 
     response = requests.get(url)
 
-    if 'X-Request-Id' in response.headers:
-        x_request_id = response.headers['X-Request-Id']
-        print(x_request_id)
-    else:
-        print("X-Request-Id not found in the response headers.")
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
